739395575f4c73a41702c1fbebfbcd0883850f86
faiyaas-dev <faiyaas.automates@gmail.com>
2026-09-12 15:35:19 +0530

    feat: fix .env file

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testMatch: [
+    '<rootDir>/tests/unit/**/*.test.[jt]s?(x)',
+    '<rootDir>/tests/integration/**/*.test.[jt]s?(x)',
+  ],
+  passWithNoTests: true,
+}
